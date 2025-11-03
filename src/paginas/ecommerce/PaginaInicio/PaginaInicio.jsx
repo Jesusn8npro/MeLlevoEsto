d4b3120c5e72d417e03c566927545e8fb267afc7
Tu Nombre Aquí <tu-email@ejemplo.com>
2025-11-03 11:51:42 -0500

    chore: Limpieza total de componentes de la página de inicio\n\n- Eliminadas carpetas HeroInicio, SeccionBeneficios, SeccionCategorias, SeccionCredibilidad y SeccionProductosVendidos\n- PaginaInicio.jsx vaciado para comenzar nueva implementación desde cero\n- Verificado build con servidor de desarrollo

--- src/paginas/ecommerce/PaginaInicio/PaginaInicio.jsx
+++ src/paginas/ecommerce/PaginaInicio/PaginaInicio.jsx
@@ -1,29 +1,12 @@
 import React from 'react'
-import HeroInicio from '../../../componentes/inicio/HeroInicio'
-import SeccionBeneficios from '../../../componentes/inicio/SeccionBeneficios/SeccionBeneficios'
-import SeccionCategorias from '../../../componentes/inicio/SeccionCategorias/SeccionCategorias'
-import SeccionProductosVendidos from '../../../componentes/inicio/SeccionProductosVendidos/SeccionProductosVendidos'
-import SeccionCredibilidad from '../../../componentes/inicio/SeccionCredibilidad/SeccionCredibilidad'
 import './PaginaInicio.css'
 
 // Página de inicio - Landing ultra vendedora
 export default function PaginaInicio() {
+  // Página de inicio limpia: comenzaremos desde cero
   return (
-    <div className="pagina-inicio">
-      {/* Hero Section - Nuevo componente estilo Martfury */}
-      <HeroInicio />
-
-      {/* Sección de Beneficios - Estilo XStore */}
-      <SeccionBeneficios />
-
-      {/* Sección de Categorías Populares - Estilo WoodMart */}
-      <SeccionCategorias />
-
-      {/* Sección de Productos Más Vendidos - Últimos productos publicados */}
-      <SeccionProductosVendidos />
-
-      {/* Sección de Credibilidad - Ultra vendedora con testimonios y garantías */}
-      <SeccionCredibilidad />
+    <div className="pagina-inicio" aria-label="Página de inicio vacía">
+      {/* Página de inicio vacía. Construiremos nueva UI por partes. */}
     </div>
   )
 }
